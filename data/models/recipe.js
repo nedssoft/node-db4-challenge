ff4797bd9946caaadefaa256e108e3aae24a9412
Orie Chinedu <oriechinedu@gmail.com>
2019-07-18 15:02:56 +0100

    Add a function to get a recipe details

--- data/models/recipe.js
+++ data/models/recipe.js
@@ -8,6 +8,23 @@ const getRecipes = () => {
   }
 };
 
+const getShoppingList = recipe_id => {
+  try {
+    const recipe = db("recipes")
+      .where({ id: recipe_id })
+      .first()
+      .join("recipe_ingredients", "recipes.id", "recipe_ingredients.recipe_id")
+      .join("ingredients", "recipes.id", "recipe_ingredients.ingredient_id")
+      .join("steps", "recipes.id", "steps.recipe_id");
+      if (recipe) {
+        return recipe
+      }
+      return null
+  } catch (error) {
+    throw new Error(error.message)
+  }
+};
+
 
 
 
